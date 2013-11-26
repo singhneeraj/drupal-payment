520d77d99bf6cce1f13fdcc6fc6656527bea21bb
Bart Feenstra <bart@mynameisbart.com>
2013-11-26 14:56:17 +0100

    Issue #2144435 by Xano: Port the "set payment status" Rules action.

---
+++ payment/lib/Drupal/payment/Plugin/Action/SetStatus.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\payment\Plugin\Action\SetStatus.
+ */
+
+namespace Drupal\payment\Plugin\Action;
+
+use Drupal\Core\Action\ConfigurableActionBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\payment\Entity\PaymentInterface;
+use Drupal\payment\Plugin\Payment\Status\Manager;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Sets a status on a payment.
+ *
+ * @Action(
+ *   id = "payment_set_status",
+ *   label = @Translation("Set payment status"),
+ *   type = "payment"
+ * )
+ */
+class SetStatus extends ConfigurableActionBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The payment status manager.
+   *
+   * @var \Drupal\payment\Plugin\Payment\Status\Manager
+   */
+  protected $paymentStatusManager;
+
+  /**
+   * Constructor.
+   *
+   * @param array $configuration
+   *   A configuration array containing information about the plugin instance.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param array $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\payment\Plugin\Payment\Status\Manager $payment_status_manager
+   */
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, Manager $payment_status_manager) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->paymentStatusManager = $payment_status_manager;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, array $plugin_definition) {
+    return new static($configuration, $plugin_id, $plugin_definition, $container->get('plugin.manager.payment.status'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function execute(PaymentInterface $payment = NULL) {
+    if ($payment) {
+      $status = $this->paymentStatusManager->createInstance($this->configuration['payment_status_plugin_id']);
+      $payment->setStatus($status);
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration() {
+    return array(
+      'payment_status_plugin_id' => '',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, array &$form_state) {
+    $form['payment_status_plugin_id'] = array(
+      '#default_value' => $this->configuration['payment_status_plugin_id'],
+      '#options' => $this->paymentStatusManager->options(),
+      '#required' => TRUE,
+      '#title' => $this->t('Payment status'),
+      '#type' => 'select',
+    );
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, array &$form_state) {
+    $this->configuration['payment_status_plugin_id'] = $form_state['values']['payment_status_plugin_id'];
+  }
+
+}
---
+++ payment/lib/Drupal/payment/Tests/Plugin/Action/SetStatusUnitTest.php
@@ -0,0 +1,128 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\payment\Tests\Plugin\Action\SetStatusUnitTest.
+ */
+
+namespace Drupal\payment\Tests\Plugin\Action;
+
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * Tests \Drupal\payment\Plugin\Action\SetStatus.
+ */
+class SetStatusUnitTest extends UnitTestCase {
+
+  /**
+   * The action under test.
+   *
+   * @var \Drupal\payment\Plugin\Action\SetStatus|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $action;
+
+  /**
+   * The payment status manager used for testing.
+   *
+   * @var \Drupal\payment\Plugin\Payment\Status\Manager|\PHPUnit_Framework_MockObject_MockObject
+   */
+  protected $paymentStatusManager;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getInfo() {
+    return array(
+      'description' => '',
+      'name' => '\Drupal\payment\Plugin\Action\SetStatus unit test',
+      'group' => 'Payment',
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setUp() {
+    $this->paymentStatusManager = $this->getMockBuilder('\Drupal\payment\Plugin\Payment\Status\Manager')
+      ->disableOriginalConstructor()
+      ->getMock();
+
+    $configuration = array();
+    $plugin_definition = array();
+    $plugin_id = $this->randomName();
+    $this->action = $this->getMockBuilder('\Drupal\payment\Plugin\Action\SetStatus')
+      ->setConstructorArgs(array($configuration, $plugin_id, $plugin_definition, $this->paymentStatusManager))
+      ->setMethods(array('t'))
+      ->getMock();
+  }
+
+  /**
+   * Tests defaultConfiguration().
+   */
+  public function testDefaultConfiguration() {
+    $configuration = $this->action->defaultConfiguration();
+    $this->assertInternalType('array', $configuration);
+    $this->assertArrayHasKey('payment_status_plugin_id', $configuration);
+  }
+
+  /**
+   * Tests buildConfigurationForm().
+   */
+  public function testBuildConfigurationForm() {
+    $this->paymentStatusManager->expects($this->once())
+      ->method('options');
+
+    $form = array();
+    $form_state = array();
+    $form = $this->action->buildConfigurationForm($form, $form_state);
+    $this->assertInternalType('array', $form);
+    $this->assertArrayHasKey('payment_status_plugin_id', $form);
+  }
+
+  /**
+   * Tests submitConfigurationForm().
+   *
+   * @depends testBuildConfigurationForm
+   */
+  public function testSubmitConfigurationForm() {
+    $plugin_id = $this->randomName();
+    $form = array();
+    $form_state = array(
+      'values' => array(
+        'payment_status_plugin_id' => $plugin_id,
+      ),
+    );
+    $this->action->submitConfigurationForm($form, $form_state);
+    $configuration = $this->action->getConfiguration();
+    $this->assertSame($plugin_id, $configuration['payment_status_plugin_id']);
+  }
+
+  /**
+   * Tests execute().
+   */
+  public function testExecute() {
+    $plugin_id = $this->randomName();
+
+    $status = $this->getMock('\Drupal\payment\Plugin\Payment\Status\PaymentStatusInterface');
+
+    $this->paymentStatusManager->expects($this->once())
+      ->method('createInstance')
+      ->with($plugin_id)
+      ->will($this->returnValue($status));
+
+    $payment = $this->getMockBuilder('\Drupal\payment\Entity\Payment')
+      ->disableOriginalConstructor()
+      ->getMock();
+    $payment->expects($this->once())
+      ->method('setStatus')
+      ->with($status);
+
+    $this->action->setConfiguration(array(
+      'payment_status_plugin_id' => $plugin_id,
+    ));
+
+    // Test execution without an argument to make sure it fails silently.
+    $this->action->execute();
+    $this->action->execute($payment);
+  }
+}
