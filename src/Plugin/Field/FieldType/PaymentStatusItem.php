ebe3d28420f7db7694ab8378bcec2d57789ab211
Bart Feenstra <bart@mynameisbart.com>
2015-04-24 07:44:32 +0300

    Issue #2455413 by Xano: Extend PaymentStatusItem::schema() with a timestamp field

--- src/Plugin/Field/FieldType/PaymentStatusItem.php
+++ src/Plugin/Field/FieldType/PaymentStatusItem.php
@@ -7,6 +7,8 @@
 
 namespace Drupal\payment\Plugin\Field\FieldType;
 
+use Drupal\Core\Field\FieldStorageDefinitionInterface;
+use Drupal\Core\TypedData\DataDefinition;
 use Drupal\payment\Payment;
 
 /**
@@ -25,6 +27,34 @@ class PaymentStatusItem extends PaymentAwarePluginBagItemBase {
   public function getPluginManager() {
     // @todo Unit-test this.
     return Payment::statusManager();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function propertyDefinitions(FieldStorageDefinitionInterface $field_definition) {
+    $properties = parent::propertyDefinitions($field_definition);
+
+    $properties['created'] = DataDefinition::create('payment_status_created')
+      ->setLabel(t('Created'));
+
+    return $properties;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function schema(FieldStorageDefinitionInterface $field_definition) {
+    $schema = parent::schema($field_definition);
+
+    $schema['columns']['created'] = [
+      'type' => 'int',
+      'not null' => TRUE,
+      'default' => 0,
+      'unsigned' => TRUE,
+    ];
+
+    return $schema;
   }
 
 }
